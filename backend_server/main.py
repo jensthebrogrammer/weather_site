c96a5656f77e649c111cb0155acf756c486b9269
jensthebrogrammer <jenslaurijssen@gmail.com>
2024-10-31 11:04:56 +0100

    iproved the syntax in webscraper and layed the ground work for the backend

--- backend_server/main.py
+++ backend_server/main.py
@@ -1,3 +1,27 @@
-from models import TodayData
-from config import db, app
+# voorlopig gaan we nog geen database aanmaken. we gaan gewoon via de backend de data halen en doorgeven.
+# op die manier hoeven we geen gebruik te maken van een database. als we later deze data willen bijhouden of analyseren.
+# dan moeten we wel een database aanmaken.
+
+from config import app
 from flask import request, jsonify
+from webscraper.webscraper import Webscraper
+
+
+@app.route("/get_day_weather", methods=['GET'])
+def get_day_weather():
+    url = request.json.get("url")
+
+    scraper = Webscraper(url)
+    actions = [scraper.get_daily_forecast, scraper.get_graph_data]
+
+    try:
+        data = scraper.use_driver(actions)
+    except Exception as e:
+        print(f'the following error occurred while getting the requested data: {e}')
+        return jsonify({'message': "a problem occurred"}), 400
+
+    return jsonify({"data": data}), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
