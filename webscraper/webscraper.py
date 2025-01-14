f926ba1c08b32f307f5ec3bf404ea39c6f0bf437
jensthebrogrammer <jenslaurijssen@gmail.com>
2025-01-14 10:01:25 +0100

    improved the way i fetch the data in the webscraper. the program is now more reliable and faster

--- webscraper/webscraper.py
+++ webscraper/webscraper.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup as Bs
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from collections import defaultdict
 
 
 class Webscraper:       # classe die alle data gaat vinden en formatten
@@ -14,72 +15,30 @@ class Webscraper:       # classe die alle data gaat vinden en formatten
         data_container = self.soup.find_all('div', class_='flex overflow-x-scroll')
 
         # create empty containers
-        time_of_day = []
-        temp_of_time = []
-        rain_of_time = []
+        data_map = defaultdict(object)
 
         for scroll_bar in data_container:   # scroll_bar is the scrolling bar on the site
-            bar = scroll_bar.find_all('div', class_='items-center')     # one of the bars
-            for data_block in bar:      # a single block in the scrolling bar
-                try:
-                    # get the time
-                    time_ = data_block.find('span', class_='text-sm')
-                    for a_time in time_:
-                        if a_time and len(a_time.text) > 4:     # to filter out the wrong data
-                            time_of_day.append(a_time.text)
-                        else:
-                            time_of_day.append(None)        # None has to be added so that the other data stays in sync
-                except Exception as e:
-                    print(f'the following error occurred when trying to get the times: {e}')
-                    return
-
-                try:
-                    # get the temp
-                    temp = data_block.find('span', class_='text-secondary')
-                    if temp:        # if it contains data
-                        temp_of_time.append(temp.text)
-                    else:
-                        temp_of_time.append(None)
-                except Exception as e:
-                    print(f'the following error occurred when trying to get the temps: {e}')
-                    return
-
-                try:
-                    # get the rain
-                    rain_div = data_block.find('div', class_='flex items-center gap-1 text-secondary h-8')
-                    if rain_div:        # if the container of the data contains something
-                        rain_span = rain_div.find('span', class_='text-sm')
-                        if rain_span:   # just checking if the data is valid
-                            rain_of_time.append(rain_span.text)
-                        else:
-                            rain_of_time.append(None)
-                    else:
-                        rain_of_time.append(None)
-                except Exception as e:
-                    print(f'the following error occurred when trying to get the rain data: {e}')
-                    return
-
             try:
-                # filtering the data
-                i = 0
-                while i < len(time_of_day):
-                    # if all the data in the index is invalid
-                    if not time_of_day[i] and not temp_of_time[i] and not rain_of_time[i]:
-                        # remove the data
-                        time_of_day.pop(i)
-                        temp_of_time.pop(i)
-                        rain_of_time.pop(i)
-                    i += 1
+                bar = scroll_bar.find_all('div', class_='items-center')     # one of the bars
+                for data_block in bar:      # a single block in the scrolling bar
+                    time = data_block.find('span', class_='text-sm') or None
+                    time = (time and data_block.
+                            find('span', class_='text-sm').text.strip())
 
-                # formatting the data
-                data_to_return = {}
-                for i in range(len(time_of_day)):
-                    data_to_return[time_of_day[i]] = dict(temp=temp_of_time[i], rain=rain_of_time[i])
+                    rain = (data_block.find('div', class_='flex items-center gap-1 text-secondary h-8')
+                            or None)
+                    rain = (rain and data_block.
+                            find('div', class_='flex items-center gap-1 text-secondary h-8').text.strip())
 
-                return data_to_return
+                    temparature = data_block.find('span', class_='text-secondary') or None
+                    temparature = (temparature and data_block.
+                                   find('span', class_='text-secondary').text.strip())
+
+                    data_map[time] = {'temp': temparature, 'rain': rain}
             except Exception as e:
-                print(f'the following error occurred when trying to format the data: {e}')
-                return
+                print(f'an error occured while mapping the data: {e}')
+
+            return data_map
 
     def get_graph_data(self):
         try:
